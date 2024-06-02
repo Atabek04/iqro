db22bebb40fa3f295a7afed14ca264c0f90ed9a4
Atabek <otabek.shadimatov@gmail.com>
2024-06-02 19:47:13 +0500

    Initialized project

---
+++ src/main/java/kz/org/iqro/iqro/IqroApplication.java
@@ -0,0 +1,13 @@
+package kz.org.iqro.iqro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IqroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IqroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kz/org/iqro/iqro/IqroApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.org.iqro.iqro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IqroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
